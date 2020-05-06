d354c8fd084edadedc8597f219f7bfe6caabb256
Scott Geryol <sgeryol@gmail.com>
2020-05-05 20:58:24 -0500

    fixed bug in updated feature

--- src/views/Update.vue
+++ src/views/Update.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
     <h1>Update your resume:</h1>
-    <h1>All Experiences</h1>
+    <h1>Experience</h1>
+    <!-- <div v-for="resume in resumes"> -->
     <div>
       Start Date:
       <input type="text" v-model="experience.start_date" />
@@ -31,7 +32,7 @@
     <div>
       Name:
       <input type="text" v-model="project.name" />
-      Description
+      Description:
       <input type="text" v-model="project.description" />
       URL:
       <input type="text" v-model="project.url" />
@@ -44,6 +45,7 @@
     </div>
     <button v-on:click="updateResume(resume)">Update Resume</button>
   </div>
+  <!-- </div> -->
 </template>
 
 <style>
@@ -56,20 +58,30 @@ export default {
   data: function() {
     return {
       experience: "",
+      education: "",
       project: "",
       skill: "",
-      education: "",
     };
   },
   created: function() {},
   methods: {
     updateResume: function(resume) {
       var params = {
-        start_date: experience.start_date,
-        end_date: experience.end_date,
-        job_title: experience.job_title,
-        company_name: experience.company_name,
-        details: experience.details,
+        start_date: resume.experience.start_date,
+        end_date: resume.experience.end_date,
+        job_title: resume.experience.job_title,
+        company_name: resume.experience.company_name,
+        details: resume.experience.details,
+        start_date: resume.education.start_date,
+        end_date: resume.education.end_date,
+        degree: resume.education.degree,
+        university_name: resume.education.university_name,
+        details: resume.education.details,
+        name: resume.project.name,
+        description: resume.project.name,
+        url: resume.project.url,
+        screenshot: resume.project.screenshot,
+        skill_name: resume.education.skill_name,
       };
       axios.patch("/api/resumes/" + resume.id, params).then(response => {
         this.currentResume = {};
@@ -77,4 +89,4 @@ export default {
     },
   },
 };
-</script>
+</script> 
