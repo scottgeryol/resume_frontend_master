98e3c13ac8f81c2e7cf29d2035e50d8d4b41d9b1
Scott Geryol <sgeryol@gmail.com>
2020-05-05 20:08:44 -0500

    initial commit for update action

--- src/views/Update.vue
+++ src/views/Update.vue
@@ -1,6 +1,48 @@
 <template>
   <div class="home">
-    <div>{{ message }}</div>
+    <h1>Update your resume:</h1>
+    <h1>All Experiences</h1>
+    <div>
+      Start Date:
+      <input type="text" v-model="experience.start_date" />
+      End Date:
+      <input type="text" v-model="experience.end_date" />
+      Job Title:
+      <input type="text" v-model="experience.job_title" />
+      Company Name:
+      <input type="text" v-model="experience.company_name" />
+      Details:
+      <input type="text" v-model="experience.details" />
+    </div>
+    <h1>Education</h1>
+    <div>
+      Start Date:
+      <input type="text" v-model="education.start_date" />
+      End Date:
+      <input type="text" v-model="education.end_date" />
+      Degree:
+      <input type="text" v-model="education.degree" />
+      University:
+      <input type="text" v-model="education.university_name" />
+      Details:
+      <input type="text" v-model="education.details" />
+    </div>
+    <h1>Projects</h1>
+    <div>
+      Name:
+      <input type="text" v-model="project.name" />
+      Description
+      <input type="text" v-model="project.description" />
+      URL:
+      <input type="text" v-model="project.url" />
+      Screenshot:
+      <input type="text" v-model="project.screenshot" />
+    </div>
+    <h1>Skills</h1>
+    <div>
+      <input type="text" v-model="education.skill_name" />
+    </div>
+    <button v-on:click="updateResume(resume)">Update Resume</button>
   </div>
 </template>
 
@@ -13,10 +55,26 @@ import axios from "axios";
 export default {
   data: function() {
     return {
-      message: "Update your resume!",
+      experience: "",
+      project: "",
+      skill: "",
+      education: "",
     };
   },
   created: function() {},
-  methods: {},
+  methods: {
+    updateResume: function(resume) {
+      var params = {
+        start_date: experience.start_date,
+        end_date: experience.end_date,
+        job_title: experience.job_title,
+        company_name: experience.company_name,
+        details: experience.details,
+      };
+      axios.patch("/api/resumes/" + resume.id, params).then(response => {
+        this.currentResume = {};
+      });
+    },
+  },
 };
 </script>
